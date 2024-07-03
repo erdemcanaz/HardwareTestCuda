c26a0633b6005e46fed63480dc0945b539cd725d
Erdem <erdemcanaz@gmail.com>
2024-07-03 16:24:09 +0300

    Update test_equipment_detection_model_webcam.py

--- test_equipment_detection_model_webcam.py
+++ test_equipment_detection_model_webcam.py
@@ -8,7 +8,7 @@ script_folder = script_path.parent.absolute()
 
 model_path = script_folder / "yolo_models" / "net_google_mask_28_06_2024.pt"
 model = YOLO(model_path)  # pretrained YOLOv8n model
-results = model(source=0, vid_stride=1, show=True, save=False, project=None, conf=0.5,stream = True)  # use webcam as source
+#results = model(source=0, vid_stride=1, show=True, save=False, project=None, conf=0.5,stream = True)  # use webcam as source
 
 
 # start webcam
