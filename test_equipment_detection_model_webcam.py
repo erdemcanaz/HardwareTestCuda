cef33f0140139d512bf4927eadcf357c9d4b2396
Erdem <erdemcanaz@gmail.com>
2024-07-03 16:26:58 +0300

    Update test_equipment_detection_model_webcam.py

--- test_equipment_detection_model_webcam.py
+++ test_equipment_detection_model_webcam.py
@@ -22,40 +22,40 @@ classNames = ["white_net", "blue_net", "safety_goggles", "blue_surgical_mask", "
 
 while True:
     success, img = cap.read()
-    results = model(img, show=True, save=False, project=None, conf=0.5,stream = True)
+    results = model(img, show=False, save=False, project=None, conf=0.5,stream = True)
 
-    # # coordinates
-    # for r in results:
-    #     boxes = r.boxes
+    # coordinates
+    for r in results:
+        boxes = r.boxes
 
-    #     for box in boxes:
-    #         # bounding box
-    #         x1, y1, x2, y2 = box.xyxy[0]
-    #         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-    #         # put box in cam
-    #         cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-    #         # confidence
-    #         confidence = math.ceil((box.conf[0]*100))/100
-    #         print("Confidence --->",confidence)
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
 
-    #         # class name
-    #         cls = int(box.cls[0])
-    #         print("Class name -->", classNames[cls])
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
 
-    #         # object details
-    #         org = [x1, y1]
-    #         font = cv2.FONT_HERSHEY_SIMPLEX
-    #         fontScale = 1
-    #         color = (255, 0, 0)
-    #         thickness = 2
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+            text = f'{classNames[cls]} {confidence}'
+            img = cv2.putText(img, text, tuple(org), font, fontScale, color, thickness, cv2.LINE_AA)                        
 
-    #         #cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
-
-    # cv2.imshow('Webcam', img)
-    # if cv2.waitKey(1) == ord('q'):
-    #     break
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
