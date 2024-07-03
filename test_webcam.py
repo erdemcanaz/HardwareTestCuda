b53cfadde124b40a0d01e4a23a388c0c48b1ce4f
Erdem <erdemcanaz@gmail.com>
2024-07-03 15:50:56 +0300

    Update test_webcam.py

--- test_webcam.py
+++ test_webcam.py
@@ -4,6 +4,11 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 
+desired_width = 640
+desired_height = 480
+cap.set(3, desired_width)  # Set the width of the frame
+cap.set(4, desired_height)  # Set the height of the frame
+
 while True:
     # Read a frame from the webcam
     ret, frame = cap.read()
